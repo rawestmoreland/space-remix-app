fc2c7c5f493afa9c97b4cb66e35e5dc94607241b
Richard Westmoreland <rawestmoreland@gmail.com>
2024-10-08 18:46:40 +0100

    feat: analytics

--- app/root.tsx
+++ app/root.tsx
@@ -33,6 +33,19 @@ export function Layout({ children }: { children: React.ReactNode }) {
         <meta name='viewport' content='width=device-width, initial-scale=1' />
         <Meta />
         <Links />
+        <script
+          async
+          src='https://www.googletagmanager.com/gtag/js?id=G-NTNMMX927V'
+        ></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: ` window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+
+    gtag('config', 'G-NTNMMX927V');`,
+          }}
+        />
       </head>
       <body>
         <div className='flex min-h-screen flex-col justify-center'>
