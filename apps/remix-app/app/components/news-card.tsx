ec9e528debc47c88b86c9cdf5907cb9c1e089ef2
Richard Westmoreland <rawestmoreland@gmail.com>
2024-10-13 18:03:49 +0100

    chore: news card style

--- apps/remix-app/app/components/news-card.tsx
+++ apps/remix-app/app/components/news-card.tsx
@@ -1,32 +1,39 @@
-import { IArticle } from "~/services/newsService";
-import { Card, CardContent, CardFooter } from "./ui/card";
-import { Badge } from "./ui/badge";
+import { IArticle } from '~/services/newsService';
+import { Card, CardContent, CardFooter } from './ui/card';
+import { Badge } from './ui/badge';
 
 export function NewsCard({ article }: { article: IArticle }) {
   return (
     <a
       key={article.id}
       href={article.url}
-      rel="noopener noreferrer"
-      target="_blank"
-      className="group h-full"
+      rel='noopener noreferrer'
+      target='_blank'
+      className='group h-full'
     >
-      <Card className="flex h-full flex-col overflow-hidden transition-all duration-300 hover:shadow-lg">
-        <div className="relative h-48 overflow-hidden">
+      <Card className='flex h-full flex-col overflow-hidden transition-all duration-300 hover:shadow-lg'>
+        <div className='relative h-48 overflow-hidden'>
           <img
-            src={article.image_url ?? "/placeholder-rocket.jpg"}
+            src={article.image_url ?? '/placeholder-rocket.jpg'}
             alt={article.title}
-            className="h-full w-full object-cover transition-transform duration-300 group-hover:scale-105"
+            className='h-full w-full object-cover transition-transform duration-300 group-hover:scale-105'
           />
         </div>
-        <CardContent className="flex-grow p-4">
-          <h3 className="mb-2 line-clamp-2 text-xl font-bold">
-            {article.title}
-          </h3>
-          <p className="line-clamp-3 text-sm">{article.summary}</p>
+        <CardContent className='flex-grow p-4'>
+          <div className='flex flex-col mb-2 gap-1'>
+            <h3 className='line-clamp-2 text-xl font-bold'>{article.title}</h3>
+            <p className='text-muted-foreground text-xs'>
+              {new Date(article.published_at).toLocaleDateString('en-US', {
+                year: 'numeric',
+                month: 'long',
+                day: 'numeric',
+              })}
+            </p>
+          </div>
+          <p className='line-clamp-3 text-sm'>{article.summary}</p>
         </CardContent>
-        <CardFooter className="z-10 bg-background pt-4">
-          <Badge variant="default">{article.news_site}</Badge>
+        <CardFooter className='z-10 bg-background pt-4'>
+          <Badge variant='default'>{article.news_site}</Badge>
         </CardFooter>
       </Card>
     </a>
