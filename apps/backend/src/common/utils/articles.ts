e267ba7bb14ed1b2df53736c151639d583bbed23
Richard Westmoreland <rawestmoreland@gmail.com>
2024-10-14 12:07:25 +0100

    feat: fetch and scrape blogs

--- apps/backend/src/common/utils/articles.ts
+++ apps/backend/src/common/utils/articles.ts
@@ -1,4 +1,5 @@
 import { prisma } from '@/prisma';
+import { ArticleType } from '@prisma/client';
 import axios from 'axios';
 
 export async function fetchArticles({
@@ -34,9 +35,45 @@ export async function fetchArticles({
   }
 }
 
+export async function fetchBlogPosts({
+  limit = 40,
+  afterDate,
+  searchTerm,
+}: {
+  limit?: number;
+  afterDate?: Date;
+  searchTerm?: string;
+}) {
+  const url = new URL(process.env.SF_NEWS_BASE_URL!);
+
+  // Build the URL
+  url.pathname += '/blogs';
+  url.searchParams.append('ordering', '-published_at');
+  url.searchParams.append('limit', limit.toString());
+  if (afterDate) {
+    url.searchParams.append('published_at_gt', afterDate.toISOString());
+  }
+  if (searchTerm) {
+    url.searchParams.append('search', searchTerm);
+  }
+
+  try {
+    const response = await axios.get(url.toString());
+
+    const articles = response.data;
+    return articles;
+  } catch (error) {
+    console.error(`Error fetching blogs: ${error}`);
+    return null;
+  }
+}
+
 export async function getLatestArticleDate() {
   try {
     const latestArticle = await prisma.article.findFirst({
+      where: {
+        articleType: ArticleType.ARTICLE,
+      },
       orderBy: {
         publishedAt: 'desc',
       },
@@ -45,6 +82,24 @@ export async function getLatestArticleDate() {
     return latestArticle?.publishedAt;
   } catch (error) {
     console.error(`Error fetching latest article date: ${error}`);
+    return undefined;
+  }
+}
+
+export async function getLatestBlogPostDate() {
+  try {
+    const latestBlogPost = await prisma.article.findFirst({
+      where: {
+        articleType: ArticleType.BLOG,
+      },
+      orderBy: {
+        publishedAt: 'desc',
+      },
+    });
+
+    return latestBlogPost?.publishedAt;
+  } catch (error) {
+    console.error(`Error fetching latest blog post date: ${error}`);
     return undefined;
   }
 }
--- apps/backend/src/trigger/scrape.ts
+++ apps/backend/src/trigger/scrape.ts
@@ -1,4 +1,9 @@
-import { fetchArticles, getLatestArticleDate } from '@/common/utils/articles';
+import {
+  fetchArticles,
+  fetchBlogPosts,
+  getLatestArticleDate,
+  getLatestBlogPostDate,
+} from '@/common/utils/articles';
 import { articleScraper } from '@/common/utils/articleScraper';
 import { prisma } from '@/prisma';
 import { ArticleSource, ArticleType } from '@prisma/client';
@@ -13,55 +18,106 @@ export const scrapeNewArticles = schedules.task({
     let articleCount = 0;
     const latestArticleDate = await getLatestArticleDate();
 
+    const latestBlogPostDate = await getLatestBlogPostDate();
+
+    const latestBlogPosts = await fetchBlogPosts({
+      afterDate: latestBlogPostDate,
+    });
+
     const latestArticles = await fetchArticles({
       afterDate: latestArticleDate,
     });
 
-    if (!latestArticles?.results) {
-      return logger.log("Couldn't find any new articles");
+    if (!latestArticles?.results && !latestBlogPosts?.results) {
+      return logger.log("Couldn't find any new articles or blog posts");
     }
 
-    for (const article of latestArticles.results) {
-      if (article.url) {
-        const existingArticle = await prisma.article.findFirst({
-          where: {
-            url: article.url,
-          },
-        });
+    if (latestArticles.results) {
+      for (const article of latestArticles.results) {
+        if (article.url) {
+          const existingArticle = await prisma.article.findFirst({
+            where: {
+              url: article.url,
+            },
+          });
 
-        // If the article already exists, skip it
-        if (existingArticle) {
-          continue;
-        }
+          // If the article already exists, skip it
+          if (existingArticle) {
+            continue;
+          }
 
-        const data = await articleScraper(article.url);
+          const data = await articleScraper(article.url);
 
-        // If the article could not be scraped, skip it
-        if (!data || data.success === false) {
-          continue;
-        }
-        // Create the article in the database
-        await prisma.article.create({
-          data: {
-            title: article.title,
-            url: article.url,
-            publishedAt: new Date(article.published_at),
-            source: ArticleSource.SPACEFLIGHT_NEWS_API,
-            articleType: ArticleType.ARTICLE,
-            newsSite: article.news_site,
-            sourceId: article.id,
-            ScrapedArticle: {
-              create: {
-                content: data.message,
+          // If the article could not be scraped, skip it
+          if (!data || data.success === false) {
+            continue;
+          }
+          // Create the article in the database
+          await prisma.article.create({
+            data: {
+              title: article.title,
+              url: article.url,
+              publishedAt: new Date(article.published_at),
+              source: ArticleSource.SPACEFLIGHT_NEWS_API,
+              articleType: ArticleType.ARTICLE,
+              newsSite: article.news_site,
+              sourceId: article.id,
+              ScrapedArticle: {
+                create: {
+                  content: data.message,
+                },
               },
             },
-          },
-        });
+          });
 
-        articleCount++;
+          articleCount++;
+        }
       }
     }
 
-    logger.log(`${articleCount} articles scraped`);
+    if (latestBlogPosts.results) {
+      for (const blogPost of latestBlogPosts.results) {
+        if (blogPost.url) {
+          const existingPost = await prisma.article.findFirst({
+            where: {
+              url: blogPost.url,
+            },
+          });
+
+          // If the article already exists, skip it
+          if (existingPost) {
+            continue;
+          }
+
+          const data = await articleScraper(blogPost.url);
+
+          // If the article could not be scraped, skip it
+          if (!data || data.success === false) {
+            continue;
+          }
+          // Create the article in the database
+          await prisma.article.create({
+            data: {
+              title: blogPost.title,
+              url: blogPost.url,
+              publishedAt: new Date(blogPost.published_at),
+              source: ArticleSource.SPACEFLIGHT_NEWS_API,
+              articleType: ArticleType.BLOG,
+              newsSite: blogPost.news_site,
+              sourceId: blogPost.id,
+              ScrapedArticle: {
+                create: {
+                  content: data.message,
+                },
+              },
+            },
+          });
+
+          articleCount++;
+        }
+      }
+    }
+
+    logger.log(`${articleCount} articles / blogs scraped`);
   },
 });
