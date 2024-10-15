11cbfaf6e33d9b27a73b29030e6947d04c47f3ef
Richard Westmoreland <rawestmoreland@gmail.com>
2024-10-15 14:31:39 +0100

    fix: version 1.0

--- apps/remix-app/app/routes/[rss.xml].tsx
+++ apps/remix-app/app/routes/[rss.xml].tsx
@@ -49,7 +49,7 @@ export function generateRss({
   link: string;
   posts: RssPost[];
 }): string {
-  const rssHeader = `<?xml version="2.0" encoding="UTF-8"?>
+  const rssHeader = `<?xml version="1.0" encoding="UTF-8"?>
     <rss xmlns:content="http://purl.org/rss/1.0/modules/content/" xmlns:dc="http://purl.org/dc/elements/1.1/" xmlns:atom="http://www.w3.org/2005/Atom" version="2.0">
       <channel>
         <title>${escapeXml(title)}</title>
