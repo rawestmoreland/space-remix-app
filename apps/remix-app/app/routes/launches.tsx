f723893006bf117e4124386d6bc816aa0f43f125
Richard Westmoreland <rawestmoreland@gmail.com>
2024-10-19 18:40:11 +0100

    chore: sort launches by net

--- apps/remix-app/app/routes/launches.tsx
+++ apps/remix-app/app/routes/launches.tsx
@@ -28,7 +28,7 @@ export async function loader({ request }: ClientLoaderFunctionArgs) {
 
   queryURL.searchParams.append('offset', offset);
   queryURL.searchParams.append('limit', limit);
-  queryURL.searchParams.append('ordering', '-net');
+  queryURL.searchParams.append('ordering', 'net');
 
   const { data, error } = await getLaunches(queryURL.toString());
   if (error) {
