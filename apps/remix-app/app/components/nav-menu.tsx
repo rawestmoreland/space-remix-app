bbedd55869a6a7c3b20bb2708a616beb6f91f7ee
Richard Westmoreland <rawestmoreland@gmail.com>
2024-10-22 18:30:02 +0100

    chore: use filter when throttled

--- apps/remix-app/app/components/nav-menu.tsx
+++ apps/remix-app/app/components/nav-menu.tsx
@@ -10,57 +10,59 @@ import {
 import { Link, NavLink } from '@remix-run/react';
 
 export function NavMenu({ throttled }: { throttled: boolean }) {
-  if (throttled) return;
-
   return (
     <NavigationMenu>
       <NavigationMenuList className='flex gap-3'>
-        {navLinks.map((link: INavLink) =>
-          !link.content?.length ? (
-            <NavigationMenuItem
-              key={link.href}
-              className='text-sm underline-offset-4 hover:underline'
-            >
-              <NavLink
-                className={({ isActive }) =>
-                  `${isActive ? 'font-medium text-primary underline' : ''}`
-                }
-                to={link.href}
+        {navLinks
+          .filter((link: INavLink) =>
+            throttled ? link.href === '/news' || link.href === '/' : true
+          )
+          .map((link: INavLink) =>
+            !link.content?.length ? (
+              <NavigationMenuItem
+                key={link.href}
+                className='text-sm underline-offset-4 hover:underline'
               >
-                {link.label}
-              </NavLink>
-            </NavigationMenuItem>
-          ) : (
-            <NavigationMenuItem key={link.href}>
-              <NavigationMenuTrigger>
                 <NavLink
-                  to={link.href}
                   className={({ isActive }) =>
-                    `${isActive ? 'text-primary underline' : ''}`
+                    `${isActive ? 'font-medium text-primary underline' : ''}`
                   }
+                  to={link.href}
                 >
                   {link.label}
                 </NavLink>
-              </NavigationMenuTrigger>
-              <NavigationMenuContent>
-                <ul className='z-50 flex min-w-max flex-col gap-4 p-4'>
-                  {link.content
-                    .sort((a, b) => a.order - b.order)
-                    .map((sublink) => (
-                      <NavigationMenuLink key={sublink.href} asChild>
-                        <li
-                          key={sublink.href}
-                          className='text-sm underline-offset-4 hover:underline'
-                        >
-                          <Link to={sublink.href}>{sublink.label}</Link>
-                        </li>
-                      </NavigationMenuLink>
-                    ))}
-                </ul>
-              </NavigationMenuContent>
-            </NavigationMenuItem>
-          )
-        )}
+              </NavigationMenuItem>
+            ) : (
+              <NavigationMenuItem key={link.href}>
+                <NavigationMenuTrigger>
+                  <NavLink
+                    to={link.href}
+                    className={({ isActive }) =>
+                      `${isActive ? 'text-primary underline' : ''}`
+                    }
+                  >
+                    {link.label}
+                  </NavLink>
+                </NavigationMenuTrigger>
+                <NavigationMenuContent>
+                  <ul className='z-50 flex min-w-max flex-col gap-4 p-4'>
+                    {link.content
+                      .sort((a, b) => a.order - b.order)
+                      .map((sublink) => (
+                        <NavigationMenuLink key={sublink.href} asChild>
+                          <li
+                            key={sublink.href}
+                            className='text-sm underline-offset-4 hover:underline'
+                          >
+                            <Link to={sublink.href}>{sublink.label}</Link>
+                          </li>
+                        </NavigationMenuLink>
+                      ))}
+                  </ul>
+                </NavigationMenuContent>
+              </NavigationMenuItem>
+            )
+          )}
       </NavigationMenuList>
     </NavigationMenu>
   );
